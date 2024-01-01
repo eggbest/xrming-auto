dd11344f9ab3f7589a0e57e149a52d34491caf34
eggbest <88075600+eggbest@users.noreply.github.com>
2024-01-01 23:33:34 +0800

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git clone https://github.com/eggbest/xrming-auto.git
-mv auto_run_script.service /etc/systemd/system/
+mv /root/xrming-auto/auto_run_script.service /etc/systemd/system/
 systemctl daemon-reload
 systemctl enable auto_run_script.service
 chmod 754 /root/xrming-auto/run.sh
