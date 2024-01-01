b018b57f98c3719a3048c458b1358d4bc275e057
eggbest <88075600+eggbest@users.noreply.github.com>
2024-01-01 10:31:00 -0500

    Add files via upload

---
+++ run2.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+git clone https://github.com/eggbest/xrming-auto.git
+mv auto_run_script.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable auto_run_script.service
+chmod 754 /root/xrming-auto/run.sh
+
+bash /root/xrming-auto/run.sh
